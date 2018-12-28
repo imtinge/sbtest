f884760be0a99e72e6b88a69e5bcf6e4a9ef02ec
imtinge <imtinge@163.com>
2018-12-28 16:30:10 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-cn">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'SbTes') -- 记录一生</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="" class="navbar-brand">Sb Test</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item">
+          <a href="/help" class="nav-link">帮助</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">登陆</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
